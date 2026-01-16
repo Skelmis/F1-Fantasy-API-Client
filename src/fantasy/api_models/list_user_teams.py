3b9a7bbfc8fb4f6fb5036a3163777e86f17d65a2
skelmis <skelmis.craft@gmail.com>
2026-01-16 18:52:11 +1300

    fix: some typings, i.e. allow nullable

--- src/fantasy/api_models/list_user_teams.py
+++ src/fantasy/api_models/list_user_teams.py
@@ -7,72 +7,72 @@ from fantasy.api_models.encoder import URLEncodedStr
 
 class PlayeridItem(BaseModel):
     id: str
-    isfinal: int
-    iscaptain: int
-    ismgcaptain: int
-    playerpostion: int
+    isfinal: int | None
+    iscaptain: int | None
+    ismgcaptain: int | None
+    playerpostion: int | None
 
 
 class TeamInfo(BaseModel):
     teamBal: float
     teamVal: float
     maxTeambal: float
-    subsallowed: int
-    userSubsleft: int
+    subsallowed: int | None
+    userSubsleft: int | None
 
 
 class UserTeamItem(BaseModel):
-    gdrank: None
-    ovrank: int
-    teamno: int
+    gdrank: int | None
+    ovrank: int | None
+    teamno: int | None
     teambal: float
     teamval: Optional[float] = None
-    gdpoints: None
-    matchday: int
+    gdpoints: int | None
+    matchday: int | None
     ovpoints: float
     playerid: list[PlayeridItem]
     teamname: URLEncodedStr
-    usersubs: int
-    boosterid: None
+    usersubs: int | None
+    boosterid: int | None
     team_info: TeamInfo
-    fttourgdid: int
-    fttourmdid: int
+    fttourgdid: int | None
+    fttourmdid: int | None
     iswildcard: Optional[int]
     maxteambal: Optional[float] = None
     capplayerid: str
-    subsallowed: int
-    isaccounting: int
-    usersubsleft: int
-    extrasubscost: int
+    subsallowed: int | None
+    isaccounting: int | None
+    usersubsleft: int | None
+    extrasubscost: int | None
     islateonboard: Optional[int]
-    mgcapplayerid: None
-    race_category: None
-    finalfxracecat: None
-    finalfxraceday: None
+    mgcapplayerid: int | None
+    race_category: int | None
+    finalfxracecat: int | None
+    finalfxraceday: int | None
     isboostertaken: Optional[int]
-    extradrstakengd: None
-    finalfixtakengd: None
-    isextradrstaken: int
-    isfinalfixtaken: int
-    issystemnameupd: int
-    iswildcardtaken: int
-    wildcardtakengd: int
-    autopilottakengd: None
-    isautopilottaken: int
-    islimitlesstaken: int
-    limitlesstakengd: None
-    isnonigativetaken: int
-    iswebpurifycalled: int
+    extradrstakengd: int | None
+    finalfixtakengd: int | None
+    isextradrstaken: int | None
+    isfinalfixtaken: int | None
+    issystemnameupd: int | None
+    iswildcardtaken: int | None
+    wildcardtakengd: int | None
+    autopilottakengd: int | None
+    isautopilottaken: int | None
+    islimitlesstaken: int | None
+    limitlesstakengd: int | None
+    isnonigativetaken: int | None
+    iswebpurifycalled: int | None
     nonigativetakengd: Optional[int]
     webpurifyresponse: str
-    finalfxnewplayerid: None
-    finalfxoldplayerid: None
-    player_swap_details: None
-    is_wildcard_taken_gd_id: None
-    inactive_driver_penality_points: int
+    finalfxnewplayerid: int | None
+    finalfxoldplayerid: int | None
+    player_swap_details: int | None
+    is_wildcard_taken_gd_id: int | None
+    inactive_driver_penality_points: int | None
 
 
 class CurrentUsersTeamsResponse(BaseModel):
-    mdid: int
+    mdid: int | None
     userTeam: list[UserTeamItem]
-    retval: int
+    retval: int | None
